2c2fe8f90afa1225ba00deef2d4d47e91d5c7e05
vivien <terametech@outlook.com>
2024-07-29 15:35:23 -0400

    new main program

---
+++ Z6502Main/Program.cs
@@ -0,0 +1,28 @@
+﻿using Z6502.Core.Enums;
+using Z6502.Core.Logging;
+using Z6502.Core.Processing;
+
+namespace Z6502Main
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            await Logger.InitalizeLog();
+
+            Processor processor = new(64 * 1024);
+            processor.Reset();
+
+
+            int programCounter = (int)processor.Memory.ProgramCounter.GetRegister();
+            processor.Memory.Data[programCounter] = (byte)InstructionType.LDA_IMMEDIATE;
+            processor.Memory.Data[programCounter + 1] = 0x42;
+
+            processor.Memory.Data[programCounter + 2] = (byte)InstructionType.LDA_ZEROPAGE;
+            processor.Memory.Data[programCounter + 3] = 0x84;
+            processor.Memory.Data[0x0084] = 0x42;
+
+            processor.Execute(5);
+        }
+    }
+}
